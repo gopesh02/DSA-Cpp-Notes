805e161a50d0aa2d009b6e1e53ee0dfa0f72d314
GOPESH YADAV <102067227+gopesh02@users.noreply.github.com>
2023-08-07 16:53:34 +0530

    Create 0001 - Fibbonacci Series.cpp

---
+++ 001-Basics of C++/001 - Loop and Functions/0001 - Fibbonacci Series.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int start = 0, end = 1;
+	int n;
+	cin>>n;
+	cout<start<<endl;
+	for(int i=0;i<n;i++){
+	    cout<<(start + last)<<endl;
+	    int temp=start;
+	    start = end;
+	    end+=temp;
+	}
+	
+	return 0;
+}
